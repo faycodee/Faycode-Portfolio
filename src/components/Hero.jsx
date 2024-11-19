c717a9a0a1da00a8819e2cc03da868ac38cc2b83
Faycal <faysaloumzil1@gmail.com>
2024-11-20 00:18:57 +0100

    faycal : edit nav bar

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,48 +1,60 @@
-import { motion } from 'framer-motion';
-import { Link } from 'react-router-dom';
-import { styles } from '../styles';
-import { navLinks } from '../constants';
-import { shaq, bwmap, worldmap } from '../assets';
+import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
+import { styles } from "../styles";
+import { navLinks } from "../constants";
+import { shaq, bwmap, worldmap } from "../assets";
 
 const Hero = () => {
   return (
     <>
-      <div className="absolute top-0 left-0 z-0 h-[100vh] w-screen">
-        <img
-          src={bwmap}
-          alt="world map"
-          className="w-full h-full sm:block hidden object-cover"
-        />
-      </div>
-      <div className="absolute top-0 left-0 z-0 h-[100vh] w-screen">
+      {/* <div className="absolute top-0 left-0 z-0 h-[100vh] w-screen"> */}
+
+      {/* </div> */}
+      {/* <div className="absolute top-0 left-0 z-0 h-[100vh] w-screen">
         <img
           src={worldmap}
           alt="world map"
           className="w-full h-full sm:hidden block object-cover"
         />
-      </div>
+      </div> */}
       <section
         className="relative flex sm:flex-row flex-col w-full h-screen mx-auto 
-        sm:bg-hero bg-hero-mobile overflow-hidden">
+        overflow-x-hidden"
+      >
+        <motion.video
+          src="./vid.mp4"
+          alt="world map"
+          autoPlay
+          style={{ zIndex: -5 }}
+          loop
+          muted
+          className="absolute inset-0 h-full  w-full sm:block hidden  h-full object-cover"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1, delay: 1 }}
+        ></motion.video>
         <div
           className={`absolute inset-0 sm:top-[250px] top-[150px] 
           lg:top-[150px] xl:top-[250px] ${styles.paddingX} 
           max-w-7xl mx-auto flex flex-row items-start
-          justify-between gap-3`}>
-          <div className="flex flex-col justify-center items-center mt-5 ml-3">
+          justify-between gap-3`}
+        >
+          {/* <div className="flex flex-col justify-center items-center mt-5 ml-3">
             <div className="w-5 h-5 rounded-full bg-[#0a0a0a] sm:hidden" />
             <div className="w-1 sm:h-80 h-40 bw-gradient sm:hidden" />
-          </div>
+          </div> */}
 
           <div>
             <h1
-              className={`${styles.heroHeadText} text-eerieBlack font-poppins uppercase`}>
-              Hi, I'm{' '}
+              className={`${styles.heroHeadText} text-eerieBlack font-poppins uppercase`}
+            >
+              Hi, I'm{" "}
               <span
                 className="sm:text-battleGray sm:text-[90px] 
                 text-eerieBlack text-[50px] font-mova
-                font-extrabold uppercase">
-                Shaquille
+                font-extrabold uppercase"
+              >
+                Faycode
               </span>
             </h1>
             <p className={`${styles.heroSubText} mt-2 text-eerieBlack`}>
@@ -52,19 +64,22 @@ const Hero = () => {
           </div>
           <div
             className="w-screen flex flex-col items-start 
-            justify-center sm:-ml-[3rem] xxs:mt-4"></div>
+            justify-center sm:-ml-[3rem] xxs:mt-4"
+          ></div>
 
           <div></div>
         </div>
 
         <div
           className="absolute xs:bottom-10 bottom-32 w-full 
-          flex justify-center items-center">
+          flex justify-center items-center"
+        >
           <a href="#about">
             <div
               className="w-[35px] h-[64px] rounded-3xl border-4 
-            border-french border-dim flex
-            justify-center items-start p-2">
+            border-frenc flex
+            justify-center items-start p-2"
+            >
               <motion.div
                 animate={{
                   y: [0, 24, 0],
@@ -72,7 +87,7 @@ const Hero = () => {
                 transition={{
                   duration: 1.5,
                   repeat: Infinity,
-                  repeatType: 'loop',
+                  repeatType: "loop",
                 }}
                 className="w-3 h-3 rounded-full bg-taupe mb-1"
               />
