d69cdd7fdd8cc373b9a3429f2356c71168f82d3c
Faycal <faysaloumzil1@gmail.com>
2024-11-26 08:02:52 +0100

    faycal :  add tech icons in assit

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,53 +1,59 @@
-import bwmap from './backgrounds/bw-map.jpeg';
-import nairobi from './backgrounds/nairobi.png';
-import whiteabstract from './backgrounds/white-abstract.png';
-import worldmap from './backgrounds/world-map.png';
+import bwmap from "./backgrounds/bw-map.jpeg";
+import nairobi from "./backgrounds/nairobi.png";
+import whiteabstract from "./backgrounds/white-abstract.png";
+import worldmap from "./backgrounds/world-map.png";
+import logo from "./logo/logo.png";
+import shaq from "./personal/shaq.png";
+import backend from "./icons/backend.png";
+import ux from "./icons/ux.png";
+import frontend from "./icons/frontend.png";
+import prototyping from "./icons/prototyping.png";
+import github from "./icons/github.png";
+import close from "./icons/close.png";
+import download from "./icons/download.png";
+import downloadHover from "./icons/downloadHover.png";
+import pineapple from "./icons/pineapple.png";
+import pineappleHover from "./icons/pineappleHover.png";
+import menu from "./icons/menu.png";
+import resume from "./icons/resume.png";
+import send from "./icons/send.png";
+import sendHover from "./icons/sendHover.png";
 
-import logo from './logo/logo.png';
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import graphql from "./tech/graphql.png";
+import postgresql from "./tech/postgresql.png";
+import rubyrails from "./tech/rubyrails.png";
+import agile from "./tech/agile.png";
+import bootstrap from "./tech/bootstrap.png";
+import framerm from "./tech/framerm.png";
+import githubi from "./tech/github.png";
+import gsap from "./tech/GSAP0.png";
+import mysql from "./tech/mysql.png";
+import php from "./tech/php.png";
+import python from "./tech/python.png";
+import three from "./tech/three.png";
 
-import shaq from './personal/shaq.png';
+import coverhunt from "./company/coverhunt.png";
+import dcc from "./company/dcc.png";
+import kelhel from "./company/kelhel.png";
+import microverse from "./company/microverse.png";
 
-import backend from './icons/backend.png';
-import ux from './icons/ux.png';
-import frontend from './icons/frontend.png';
-import prototyping from './icons/prototyping.png';
-import github from './icons/github.png';
-import close from './icons/close.png';
-import download from './icons/download.png';
-import downloadHover from './icons/downloadHover.png';
-import pineapple from './icons/pineapple.png';
-import pineappleHover from './icons/pineappleHover.png';
-import menu from './icons/menu.png';
-import resume from './icons/resume.png';
-import send from './icons/send.png';
-import sendHover from './icons/sendHover.png';
-
-import css from './tech/css.png';
-import docker from './tech/docker.png';
-import figma from './tech/figma.png';
-import git from './tech/git.png';
-import html from './tech/html.png';
-import javascript from './tech/javascript.png';
-import nodejs from './tech/nodejs.png';
-import reactjs from './tech/reactjs.png';
-import redux from './tech/redux.png';
-import tailwind from './tech/tailwind.png';
-import typescript from './tech/typescript.png';
-import graphql from './tech/graphql.png';
-import postgresql from './tech/postgresql.png';
-import rubyrails from './tech/rubyrails.png';
-
-import coverhunt from './company/coverhunt.png';
-import dcc from './company/dcc.png';
-import kelhel from './company/kelhel.png';
-import microverse from './company/microverse.png';
-
-import komikult from './projects/komikult.png';
-import leaderboard from './projects/leaderboard.png';
-import math from './projects/math-magicians.png';
-import movie from './projects/movie-metro.png';
-import nyeusi from './projects/nyeusi.png';
-import space from './projects/space-hub.png';
+import komikult from "./projects/komikult.png";
+import leaderboard from "./projects/leaderboard.png";
+import math from "./projects/math-magicians.png";
+import movie from "./projects/movie-metro.png";
+import nyeusi from "./projects/nyeusi.png";
+import space from "./projects/space-hub.png";
 
 export {
   bwmap,
@@ -55,7 +61,6 @@ export {
   whiteabstract,
   worldmap,
   logo,
-
   shaq,
   backend,
   ux,
@@ -72,6 +77,15 @@ export {
   send,
   sendHover,
   css,
+  agile,
+  bootstrap,
+  framerm,
+  githubi,
+  gsap,
+  mysql,
+  php,
+  python,
+  three,
   docker,
   figma,
   git,
