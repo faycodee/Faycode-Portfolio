d7b7a2301337a46fe58aad3ea3f1305b1114fa69
Faycal <faysaloumzil1@gmail.com>
2024-12-03 07:44:25 +0100

    Faycal: setup redux with this project

--- src/store/store.js
+++ src/store/store.js
@@ -1,15 +1,15 @@
 import { createStore } from "redux";
 import { produce } from "immer";
-import { Return } from "three/webgpu";
+
 
 // return `${year}-${month}-${day+Math.floor(Math.random() * 4) + 1}`;
 const mystate = {};
 const Render = (state = mystate, action) => {
   switch (action.type) {
-    case "ADD":
-      return produce(state, (draftState) => {
-        draftState.Tasks.unshift(action.newTask);
-      });
+    // case "ADD":
+    //   return produce(state, (draftState) => {
+    //     draftState.Tasks.unshift(action.newTask);
+    //   });
   }
 };
 const store = createStore(Render);
