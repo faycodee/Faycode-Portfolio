dd735c3d0c09c42f5b712ec859590e7ba27c905d
Faycal <faysaloumzil1@gmail.com>
2024-12-03 16:48:45 +0100

    Faycal: edit store add object cursor

--- src/store/store.js
+++ src/store/store.js
@@ -2,12 +2,16 @@ import { createStore } from "redux";
 import { produce } from "immer";
 
 // return `${year}-${month}-${day+Math.floor(Math.random() * 4) + 1}`;
-const mystate = {  cursorScale: "1"  };
+const mystate = {
+  cursor: {
+    scale: 1,
+  },
+};
 const Render = (state = mystate, action) => {
   switch (action.type) {
     case "UPDATECURSORSCALE":
       return produce(state, (draftState) => {
-        draftState.cursorScale.valueOf(action.scale);
+        draftState.cursor.scale.valueOf(action.scale);
       });
   }
 };
