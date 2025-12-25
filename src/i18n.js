5d4bd5b47a18508059c87b519aed63567b488e23
Faycal <faysaloumzil1@gmail.com>
2025-12-25 08:09:44 +0100

    edit default language

--- src/i18n.js
+++ src/i18n.js
@@ -1,8 +1,7 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
-
-import HttpApi from "i18next-http-backend"; // Import the backend
+import HttpApi from "i18next-http-backend";
 
 i18n
   .use(LanguageDetector)
@@ -10,16 +9,21 @@ i18n
   .use(initReactI18next)
   .init({
     debug: true,
-    fallbackLng: "gr",
+
+    // ✅ German as default & fallback
+    fallbackLng: "de",
+
     detection: {
-      cache: ["cookie"],
+      // Order matters
+      order: ["localStorage", "cookie", "navigator", "htmlTag"],
+      caches: ["localStorage", "cookie"],
     },
+
     interpolation: {
       escapeValue: false,
     },
- 
+
     backend: {
-      // Path to translation files in `public/locale`
       loadPath: "/locale/{{lng}}/translation.json",
     },
   });
